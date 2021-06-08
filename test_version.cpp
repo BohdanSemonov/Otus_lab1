7ed9a24b3076e157837f602da6c946726a8bddbf
BohdanSemonov <bohdan.semonov@gmail.com>
2021-06-08 22:36:13 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -8,7 +8,10 @@ BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    BOOST_CHECK(version() > 0);
+    BOOST_TEST_REQUIRE(boost::unit_test::framework::master_test_suite().argc == 3);
+    BOOST_TEST_REQUIRE(boost::unit_test::framework::master_test_suite().argv[1] == "--version");
+
+    BOOST_TEST(std::to_string(version()) == boost::unit_test::framework::master_test_suite().argv[2]);
 }
 
 }
