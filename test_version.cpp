e2e909520bd3518b1b62041a1ad4c620ee8ab4a8
BohdanSemonov <bohdan.semonov@gmail.com>
2021-06-09 00:03:48 +0300

    Update test_version.cpp

--- test_version.cpp
+++ test_version.cpp
@@ -4,26 +4,11 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <iostream>
-#include <fstream>
-
 BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    std::ifstream f("ver_tst");
-    std::string ver_str;
-    
-    if (f.is_open())
-    {
-        getline(f, ver_str);
-        BOOST_TEST(version() > 0);
-        f.close();
-    }
-    
-    std::ofstream fw("ver_tst");
-    fw << version() << "\n";
-    fw.close();
+    BOOST_TEST(version() > 0);
 }
 
 }
